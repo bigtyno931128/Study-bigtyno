97187f507abc27861c738b0f05a03490e8a7b82e
bigtyno931128 <bigtyno4@gmail.com>
2023-06-13 14:09:00 +0900

    java-study

--- java-study/src/Main.java
+++ java-study/src/Main.java
@@ -1,6 +1,21 @@
+// public : 제어자 (접근)
+
 public class Main {
 
+
+    // static -- > 프로그램이 실행 될 때 실행이 된다 .
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        // (1) boolean 
+        boolean flag = true;
+        
+        // (2) 문자형 (char) 
+        char alphabet = 'A';
+        System.out.println("alphabet = " + alphabet);
+        System.out.println("int _ alphabet = " + (int) alphabet);
+
+        
+
     }
 }

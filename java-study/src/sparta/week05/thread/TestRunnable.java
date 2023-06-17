4a295a605db702fb211627e370fe23472fec0f6f
bigtyno931128 <bigtyno4@gmail.com>
2023-06-17 20:12:35 +0900

    java_study

---
+++ java-study/src/sparta/week05/thread/TestRunnable.java
@@ -0,0 +1,11 @@
+package sparta.week05.thread;
+
+public class TestRunnable implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i=0; i < 100 ; i++ ){
+            System.out.print("@");
+        }
+    }
+}

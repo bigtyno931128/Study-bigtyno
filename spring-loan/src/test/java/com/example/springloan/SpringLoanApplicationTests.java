be43f5919ad338179dabed157f5a726a54a90fc0
bigtyno931128 <bigtyno4@gmail.com>
2023-06-24 23:34:06 +0900

    spring-loan

---
+++ spring-loan/src/test/java/com/example/springloan/SpringLoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
